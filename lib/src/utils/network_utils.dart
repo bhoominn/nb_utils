c5b575f69da535b5ada5e9bf94cab085940cd396
Bhoomin <bhumin@goldenmace.com>
2024-04-16 16:38:55 +0530

    - Added checkConnectivityResult method to get ConnectivityResult instead of List<ConnectivityResult>

--- lib/src/utils/network_utils.dart
+++ lib/src/utils/network_utils.dart
@@ -1,6 +1,17 @@
 import 'package:nb_utils/nb_utils.dart';
 
 /// returns true if network is available
+Future<ConnectivityResult> checkConnectivityResult() async {
+  var connectivityResult = await Connectivity().checkConnectivity();
+
+  if (!connectivityResult.contains(ConnectivityResult.none)) {
+    return connectivityResult.first;
+  } else {
+    return ConnectivityResult.none;
+  }
+}
+
+/// returns true if network is available
 Future<bool> isNetworkAvailable() async {
   var connectivityResult = await Connectivity().checkConnectivity();
   return !connectivityResult.contains(ConnectivityResult.none);
